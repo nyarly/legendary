8fb0db09ca76074fdcc2ed29b61cb1a62466baf5
Judson <nyarly@gmail.com>
2016-07-22 16:56:30 -0700

    Hitlist

--- main.go
+++ main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 	"log"
 	"os"
 	"path/filepath"
 	"regexp"
+	"sort"
+	"strconv"
+	"text/tabwriter"
 	"text/template"
 	"time"
 
@@ -19,24 +23,68 @@ import (
 
 const (
 	docstring = `Parse go coverage profiles into vim-legend files
-Usage: legendary [--project-root=<dir>] [--coverage-root=<dir>] <out_path> <coverage_path>...
+Usage: legendary [options] <out_path> <coverage_path>...
+       legendary [options] --hitlist [--limit=n] <coverage_path>...
 
 Options:
 	--coverage-root=<dir> Treat the coverage files as refering to files rooted at <dir> (Default: $GOPATH/src)
 	--project-root=<dir>  Emit the vim-legend configuration rooted at <dir> (Default: $PWD)
+	--hitlist             Don't produce vim-legend output - instead repoort on worst covered files
+	--limit=<n>           Limit the number of files in the hitlist to <n>
 `
 )
 
-type context struct {
-	Now     int64
-	Results map[string]*result
+type (
+	context struct {
+		Now     int64
+		Results map[string]*result
+	}
+
+	result struct {
+		filename string
+		lines    int
+		counts   map[int]int
+		Hits     []int
+		Misses   []int
+		Ignored  []int
+	}
+	resultsByLines      []*result
+	resultsByPercentage []*result
+)
+
+func (rz *result) MissCount() int {
+	return len(rz.Misses)
 }
 
-type result struct {
-	counts  map[int]int
-	Hits    []int
-	Misses  []int
-	Ignored []int
+func (rz *result) HitCount() int {
+	return len(rz.Hits)
+}
+
+func (rz *result) MissFraction() float64 {
+	hc, mc := rz.HitCount(), rz.MissCount()
+	return float64(mc) / float64(hc+mc)
+}
+
+func (r resultsByPercentage) Len() int {
+	return len(r)
+}
+func (r resultsByPercentage) Less(i, j int) bool {
+	return r[i].MissFraction() < r[j].MissFraction()
+}
+func (r resultsByPercentage) Swap(i, j int) {
+	n, m := r[i], r[j]
+	r[j], r[i] = n, m
+}
+
+func (r resultsByLines) Len() int {
+	return len(r)
+}
+func (r resultsByLines) Less(i, j int) bool {
+	return r[i].MissCount() < r[j].MissCount()
+}
+func (r resultsByLines) Swap(i, j int) {
+	n, m := r[i], r[j]
+	r[j], r[i] = n, m
 }
 
 func main() {
@@ -46,8 +94,6 @@ func main() {
 	}
 
 	sourceFiles := parsed[`<coverage_path>`].([]string)
-	targetPath := parsed[`<out_path>`].(string)
-
 	coverageRoot := filepath.Join(os.Getenv("GOPATH"), "src")
 	if gcr, ok := parsed[`--coverage-root`]; ok && gcr != nil {
 		coverageRoot = gcr.(string)
@@ -63,66 +109,44 @@ func main() {
 		}
 	}
 
-	var ctx context
-	ctx.Results = make(map[string]*result)
+	ctx := collectCoverageContext(coverageRoot, projRoot, sourceFiles)
 
-	for _, fp := range sourceFiles {
-		ps, err := cover.ParseProfiles(fp)
-		if err != nil {
-			log.Print(err)
-			continue
+	if parsed[`--hitlist`].(bool) {
+		var rs resultsByLines
+		for _, v := range ctx.Results {
+			rs = append(rs, v)
+		}
+		sort.Sort(sort.Reverse(rs))
+
+		var ps resultsByPercentage
+		for _, v := range ctx.Results {
+			ps = append(ps, v)
+		}
+		sort.Sort(sort.Reverse(ps))
+
+		var rps [][2]*result
+		for i := range rs {
+			rps = append(rps, [2]*result{rs[i], ps[i]})
 		}
 
-		for _, p := range ps {
-			an := filepath.Join(coverageRoot, p.FileName)
-			rn, err := filepath.Rel(projRoot, an)
-			if err != nil {
-				log.Print(err)
-				continue
-			}
-
-			r, ok := ctx.Results[rn]
-			if !ok {
-				r = &result{counts: make(map[int]int)}
-				ctx.Results[rn] = r
-			}
-			for _, pb := range p.Blocks {
-				for ln := pb.StartLine; ln <= pb.EndLine; ln++ {
-					r.counts[ln] += pb.Count
-				}
+		len := len(rps)
+		if lim := parsed["--limit"]; lim != nil {
+			l, err := strconv.ParseInt(lim.(string), 10, 0)
+			if err == nil {
+				len = int(l)
 			}
 		}
+
+		tabs := tabwriter.NewWriter(os.Stdout, 0, 2, 2, ' ', 0)
+		fmt.Fprintf(tabs, "File\tMissed Lines\tFile\tPercent missed\n")
+		for i := 0; i < len; i++ {
+			fmt.Fprintf(tabs, "%s\t%d\t%s\t%.2f\n",
+				rps[i][0].filename, rps[i][0].MissCount(), rps[i][1].filename, rps[i][1].MissFraction()*100)
+		}
+		tabs.Flush()
+		return
 	}
-
-	for f, r := range ctx.Results {
-		file, err := os.Open(filepath.Join(projRoot, f))
-		if err != nil {
-			log.Print(err)
-			delete(ctx.Results, f)
-			continue
-		}
-
-		lines, err := lineCounter(file)
-		if err != nil {
-			log.Print(err)
-			delete(ctx.Results, f)
-			continue
-		}
-
-		for ln := 0; ln < lines; ln++ {
-			c, ok := r.counts[ln]
-			switch {
-			default:
-				r.Misses = append(r.Misses, ln)
-			case !ok:
-				r.Ignored = append(r.Ignored, ln)
-			case c > 0:
-				r.Hits = append(r.Hits, ln)
-			}
-		}
-	}
-
-	ctx.Now = time.Now().Unix()
+	targetPath := parsed[`<out_path>`].(string)
 	tmpl := getTemplate("coverage.tmpl")
 
 	out := &bytes.Buffer{}
@@ -134,6 +158,68 @@ func main() {
 	tmpl.Execute(out, ctx)
 
 	file.Write(out.Bytes())
+}
+
+func missOutput(rs []*result, header string, fn func(*result) string) {
+}
+
+func injestCoverageFile(ctx *context, coverageRoot, projRoot, fp string) {
+	ps, err := cover.ParseProfiles(fp)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+
+	for _, p := range ps {
+		an := filepath.Join(coverageRoot, p.FileName)
+		rn, err := filepath.Rel(projRoot, an)
+		if err != nil {
+			log.Print(err)
+			return
+		}
+
+		r, ok := ctx.Results[rn]
+		if !ok {
+			r = &result{filename: rn, counts: make(map[int]int)}
+			ctx.Results[rn] = r
+		}
+		for _, pb := range p.Blocks {
+			for ln := pb.StartLine; ln <= pb.EndLine; ln++ {
+				r.counts[ln] += pb.Count
+			}
+		}
+	}
+	return
+}
+
+func buildFileCoverage(ctx *context, projRoot, f string, r *result) {
+	file, err := os.Open(filepath.Join(projRoot, f))
+	if err != nil {
+		log.Print(err)
+		delete(ctx.Results, f)
+		return
+	}
+
+	lines, err := lineCounter(file)
+	if err != nil {
+		log.Print(err)
+		delete(ctx.Results, f)
+		return
+	}
+	r.lines = lines
+
+	for ln := 0; ln < r.lines; ln++ {
+		c, ok := r.counts[ln]
+		switch {
+		default:
+			r.Misses = append(r.Misses, ln)
+		case !ok:
+			r.Ignored = append(r.Ignored, ln)
+		case c > 0:
+			r.Hits = append(r.Hits, ln)
+		}
+	}
+	return
 }
 
 func getTemplate(n string) *template.Template {
@@ -168,6 +254,7 @@ func lineCounter(r io.Reader) (int, error) {
 	}
 }
 
+// XXX
 type escaper struct {
 	r     io.Reader
 	old   []byte
@@ -212,4 +299,19 @@ func (e *escaper) Read(p []byte) (n int, err error) {
 
 func newEscaper(r io.Reader) *escaper {
 	return &escaper{r, make([]byte, 0), false}
+}
+func collectCoverageContext(coverageRoot string, projRoot string, sourceFiles []string) context {
+	var ctx context
+	ctx.Results = make(map[string]*result)
+
+	for _, fp := range sourceFiles {
+		injestCoverageFile(&ctx, coverageRoot, projRoot, fp)
+	}
+
+	for f, r := range ctx.Results {
+		buildFileCoverage(&ctx, projRoot, f, r)
+	}
+
+	ctx.Now = time.Now().Unix()
+	return ctx
 }
